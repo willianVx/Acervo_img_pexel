fb517be44930b5875342d83ceb1ef1035d4d5d32
willianVx <willianvoliveiravx@gmail.com>
2018-02-06 18:12:50 -0200

    adicao de calculadora de precos

---
+++ calculadoraProdutosInsta/main.php
@@ -0,0 +1,25 @@
+<?php
+    class CalculaPreco
+    {
+        private $precoBaseAcabamento = array(
+        'meta7mm' => 2467, 
+        'meta5mm' => 1771,
+        'meta4mm' => 1392,
+        'meta3mm' => 1265, 
+        'acm5mm' => 1518, 
+        'uvPS' => 1265, 
+        'uvACM' => 1553, 
+        'papelAlgodao' => 552, 
+        'papelFosco' => 714,
+        'papelAcetinato' => 460,
+        'papelBrilhante' => 437,
+        'papelCanvas' => 633,
+        );
+
+        private $precoBaseMoldura = array(
+
+        );
+
+    }
+    
+?>
