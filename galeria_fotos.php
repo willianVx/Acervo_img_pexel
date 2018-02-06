c363e32408dcd96e69ec4d6927fc3f319ecff2d9
willianVx <willianvoliveiravx@gmail.com>
2018-02-06 11:01:05 -0200

    Melhorias na apresentação das imagens do acervo

--- galeria_fotos.php
+++ galeria_fotos.php
@@ -9,14 +9,13 @@
             require 'vendor/autoload.php';
             $pexels = new \Glooby\Pexels\Client("563492ad6f91700001000001cc96a2dbc78445ba83df0b75bb703e4e");
             $response = $pexels->search($pesquisa, 40, 1);
+            echo "<pre>".$response."</pre>";
             $data = $response->json();
             foreach ($data['photos'] as $photo) {
-                
                 echo "<div class='galeria_imagens'>";
-                echo "<div class='col-lg-5 galeria_idividual_img'> <img src='".$photo['src']['medium']."'></div>";
-                //echo " Tamanho de ". $photo['width']." por ".$photo['height']." ";
+                print "<div class='col-lg-4 galeria_idividual_img'> <img src='".$photo['src']['medium']."'></div>";
+                //echo $photo['width']." por ".$photo['height'];
                 echo "</div>";
-                //echo "<p>".$photo['src']['original']."</p>";
             }
     }
 ?>
